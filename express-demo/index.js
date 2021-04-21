a596502ffe7d8f8677232fc284bff236a45c7d8b
alimuamar11 <alimuamar11@gmail.com>
2021-04-21 10:17:15 +0700

    Environment Variables

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello dunia');
+    res.send('hello dunia ku');
 });
 
 app.get('/api/courses', (req,res)=>{
     res.send([1,2,3])
 })
-app.listen(3000, () => console.log('Listening on port 3000,...'))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port},...`))
+// app.listen(3000, () => console.log('Listening on port 3000,...'))
